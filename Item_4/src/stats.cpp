7829d18e0cb17762b32a4970248bede03a7a1dd6
Calin Zavoi <brolincz@gmail.com>
2021-11-03 02:03:23 +0200

    Initial fixed

---
+++ Item_4/src/stats.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Human{
+    private:
+    unsigned health=100;
+
+    public:
+    Human(unsigned h){
+        health=h;
+    }
+};
+
+int main(){
+    std::cout<<"Initial\n";
+    return 0;
+}
