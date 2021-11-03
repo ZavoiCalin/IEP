a38f0516c622c96dc5cc9c4a7ab7086da876d6a3
Calin Zavoi <brolincz@gmail.com>
2021-11-03 21:13:48 +0200

    Delete constructs

--- Item_4/src/stats.cpp
+++ Item_4/src/stats.cpp
@@ -3,15 +3,32 @@
 class Human{
     private:
     unsigned health;
+    std::string name;
 
     public:
-    Human() : health(100) {}
-    Human(unsigned h) : health(h){
-        
+    Human() 
+    : health(100), 
+    name("Default")
+    {
+        std::cout<<"Default Human\n";
+    }
+
+    //Human()=delete;
+
+    Human(unsigned h, std::string n) 
+    : health(h),
+    name(n)
+    {}
+
+     Human(unsigned h, std::string n) =delete;
+
+    ~Human(){
+        std::cout<<"Human destroyed\n";
     }
 };
 
 int main(){
-    std::cout<<"Initial\n";
+    Human h1(333, "Primul");
+    
     return 0;
 }
